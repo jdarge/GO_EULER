2f0993b1bba2466e7ff7af92edfd7c461fe39fad
Jan D <79925808+jdarge@users.noreply.github.com>
2024-05-06 16:55:26 -0400

    Update main.go

--- main/main.go
+++ main/main.go
@@ -4,22 +4,20 @@ import (
 	"fmt"
 )
 
-
-
 func main() {
-	
-	problem_1()
-	problem_2()
-	problem_3()
-	
+
+	problem1()
+	problem2()
+	problem3()
+
 }
 
-func problem_1() {
+func problem1() {
 	var sum = 0
 
 	for i := 1; i < 1000; i++ {
-	
-		if (i%3 == 0 || i%5 == 0) {
+
+		if i%3 == 0 || i%5 == 0 {
 			sum += i
 		}
 	}
@@ -27,21 +25,21 @@ func problem_1() {
 	fmt.Printf("%v\n", sum)
 }
 
-func problem_2() {
+func problem2() {
 	var sum, tmp, f, s = 0, 0, 1, 2
 
 	for {
-		if(s > 4_000_000) {
+		if s > 4_000_000 {
 			break
 		}
 
-		if(s%2==0) {
-			sum+=s
+		if s%2 == 0 {
+			sum += s
 		}
-		
-		tmp = f+s
-		f=s
-		s=tmp
+
+		tmp = f + s
+		f = s
+		s = tmp
 	}
 
 	fmt.Printf("%v\n", sum)
@@ -54,36 +52,36 @@ func abs(n int) int { // absolute value |x|
 	return -n
 }
 
-func gcd (a,b int) int { // greatest common divisor
-	for b!=0 {
-		a,b = b,a%b
+func gcd(a, b int) int { // greatest common divisor
+	for b != 0 {
+		a, b = b, a%b
 	}
 	return a
 }
 
-func rho_g(input, n int) int {
-	return ((input*input + 1) % n)
+func rhoG(input, n int) int {
+	return (input*input + 1) % n
 }
 
 // https://en.wikipedia.org/wiki/Pollard's_rho_algorithm
-func rho (n int) int {
-	var x,y,d = 2,2,1
+func rho(n int) int {
+	var x, y, d = 2, 2, 1
 
-	for d==1 {
-		x = rho_g(x, n)
-		y = rho_g(rho_g(y, n), n)
+	for d == 1 {
+		x = rhoG(x, n)
+		y = rhoG(rhoG(y, n), n)
 		d = gcd(abs(x-y), n)
 	}
 
 	return d
 }
 
-func prime_factors(n int) ([]int, int) {
+func primeFactors(n int) ([]int, int) {
 
-	var factors = []int{}
+	var factors []int
 	var factor = 0
 	var index = 0
-	
+
 	for n > 1 {
 		factor = rho(n)
 		factors = append(factors, factor)
@@ -94,9 +92,9 @@ func prime_factors(n int) ([]int, int) {
 	return factors, index
 }
 
-func problem_3() {
+func problem3() {
 
-	var factors, index = prime_factors(600851475143)
-	var largest_factor = factors[index-1]
-	fmt.Printf("%d", largest_factor)
+	var factors, index = primeFactors(600851475143)
+	var largestFactor = factors[index-1]
+	fmt.Printf("%d", largestFactor)
 }
