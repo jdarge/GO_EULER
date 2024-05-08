8fdbb6d367286ad2cfeb1ab0fa8abfdf1c4aad9d
jdarge <79925808+jdarge@users.noreply.github.com>
2024-05-07 21:26:50 -0400

    Added problem 15 logic write up

--- problem_10-19/problem15.go
+++ problem_10-19/problem15.go
@@ -22,6 +22,27 @@ func BinomialCoefficient(c int, k int) uint64 {
 	return 0
 }
 
+/*
+
+LOGIC:
+	In problem 15 we are tasked with going from the top left to bottom right of a square using only right and down
+		movements
+	In an NxN `matrix` we are required to move N times down and N time right to get to our destination
+	In other words...
+		( 40 ) binomial coefficient
+		( 20 )
+	Here 40 represents the total amount of moves and 20 represents the amount of times we can move in a single direction
+	A binomial coefficient of
+		( c )
+		( k )
+	... can be represented as (c!)/(k!(c-k)!)
+	This is the logic used to find the answer
+
+	https://en.wikipedia.org/wiki/Binomial_coefficient
+	https://www.youtube.com/watch?v=AOsWph2FNLw
+
+*/
+
 func Problem15() {
 	answer := BinomialCoefficient(40, 20)
 	fmt.Printf("Problem 15:\t%d\n", answer)
