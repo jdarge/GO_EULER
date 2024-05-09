2f925f81084d97aab317f672550c1dfb36852bfc
jdarge <79925808+jdarge@users.noreply.github.com>
2024-05-09 02:31:00 -0400

    Added solution 6 back

--- problem_01-09/problem6.go
+++ problem_01-09/problem6.go
@@ -1,5 +1,34 @@
 package problem0109
 
-func Problem6() {
+import (
+	helper "euler/helpers"
+	"fmt"
+)
 
+func Problem6() int {
+	var tetrahedral, triangle, answer int
+
+	n := 100
+
+	triangle = helper.SumOfNumbers(n)
+	tetrahedral = helper.SumOfSquares(n)
+	answer = (triangle * triangle) - tetrahedral
+
+	fmt.Printf("%v::\n%v::\n", triangle, tetrahedral)
+
+	return answer
+}
+
+func Problem6_original() int {
+
+	sum, squares := 0, 0
+
+	for i := 1; i <= 100; i++ {
+		sum += i
+		squares += i * i
+	}
+
+	answer := (sum * sum) - squares
+
+	return answer
 }
